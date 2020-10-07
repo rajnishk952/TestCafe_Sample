a24fb5bca396a9d4aa2536d8ac53ec88dad20c64
rajnishk952 <rajnish@browserstack.com>
2020-10-07 08:50:20 +0530

    Changes related to Steps

--- config/single-run.sh
+++ config/single-run.sh
@@ -3,6 +3,7 @@
 export BROWSERSTACK_PROJECT_NAME="CE TestCafe Samples"
 export BROWSERSTACK_BUILD_ID="TestCafe samples"
 export BROWSERSTACK_DISPLAY_RESOLUTION="1024x768"
+export BROWSERSTACK_USE_AUTOMATE=1
 
 #./node_modules/.bin/testcafe -c 2 "browserstack:ie@11.0:Windows 10" ./tests/login.test.js --test-scheduling
 ./node_modules/.bin/testcafe -c 2 '@browserstack/browserstack:chrome' ./tests/login.test.js --test-scheduling
